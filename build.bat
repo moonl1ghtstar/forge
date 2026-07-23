a1a9845f9dac6a0c4e1ec82bd1a91f1d35d7f0bf
LaMoon <realmoonl1ghtstar@gmail.com>
2026-07-23 16:52:37 +0900

    Update Forge build script

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ if errorlevel 1 (
     goto fail
 )
 
-set "ROOT=%~dp0"
+set "ROOT=%DRIVE%\"
 set "SRC=%ROOT%forge\helix\src"
 set "IRSRC=%SRC%\ir"
 set "CSRC=%ROOT%forge\c\src"
@@ -23,6 +23,7 @@ set "WORKDIR=%DRIVE%\forge-build"
 set "WORKOUT=%WORKDIR%\forge.exe"
 set "TMPDIR=%WORKDIR%\temp"
 set "TEMP=%TMPDIR%"
+set "TMP=%TMPDIR%"
 
 if not exist "%BIN%" mkdir "%BIN%" || goto fail
 if not exist "%WORKDIR%" mkdir "%WORKDIR%" || goto fail
@@ -31,6 +32,13 @@ if not exist "%TMPDIR%" mkdir "%TMPDIR%" || goto fail
 where gcc >nul 2>nul
 if errorlevel 1 (
     echo Forge error: gcc not found in PATH.
+    set "RC=1"
+    goto fail
+)
+
+where ar >nul 2>nul
+if errorlevel 1 (
+    echo Forge error: ar not found in PATH.
     set "RC=1"
     goto fail
 )
@@ -46,6 +54,11 @@ gcc -std=c11 -O2 -Wall -Wextra ^
     -I"%SRC%\codegen" ^
     -I"%SRC%\errors" ^
     -I"%ROOT%forge\assembler" ^
+    -I"%ROOT%forge\assembler\src\lexer" ^
+    -I"%ROOT%forge\assembler\src\parser" ^
+    -I"%ROOT%forge\assembler\src\x86-encode" ^
+    -I"%ROOT%forge\assembler\src\coff-writer" ^
+    -I"%ROOT%forge\assembler\src\forge-asm" ^
     -I"%CSRC%" ^
     -I"%CSRC%\lexer" ^
     -I"%CSRC%\parser" ^
@@ -59,11 +72,11 @@ gcc -std=c11 -O2 -Wall -Wextra ^
     "%SRC%\ir\builder\ir-builder.c" ^
     "%SRC%\ir\opt\ir-opt.c" ^
     "%SRC%\codegen\helix-codegen.c" ^
-    "%ROOT%forge\assembler\asm-lexer.c" ^
-    "%ROOT%forge\assembler\asm-parser.c" ^
-    "%ROOT%forge\assembler\x86-encode.c" ^
-    "%ROOT%forge\assembler\coff-writer.c" ^
-    "%ROOT%forge\assembler\forge-asm.c" ^
+    "%ROOT%forge\assembler\src\lexer\asm-lexer.c" ^
+    "%ROOT%forge\assembler\src\parser\asm-parser.c" ^
+    "%ROOT%forge\assembler\src\x86-encode\x86-encode.c" ^
+    "%ROOT%forge\assembler\src\coff-writer\coff-writer.c" ^
+    "%ROOT%forge\assembler\src\forge-asm\forge-asm.c" ^
     "%SRC%\errors\forge-errors.c" ^
     "%CSRC%\lexer\c-lexer.c" ^
     "%CSRC%\parser\c-parser.c" ^
@@ -84,7 +97,20 @@ if errorlevel 1 goto fail
 copy /y "%WORKOUT%" "%OUT_4G%" >nul
 if errorlevel 1 goto fail
 
-set "MODDIR=%ROOT%module"
+echo Building standard libraries...
+gcc -std=c11 -O2 -Wall -Wextra -c "%ROOT%module\helix\console\console.c" -o "%ROOT%module\helix\console\console.obj"
+if errorlevel 1 (
+    set "RC=1"
+    goto fail
+)
+
+ar rcs "%ROOT%module\helix\console\console.lib" "%ROOT%module\helix\console\console.obj"
+if errorlevel 1 (
+    set "RC=1"
+    goto fail
+)
+
+set "MODDIR=%ROOT%module\helix"
 set "LIBDIR=%ROOT%forge\bin\lib"
 
 if not exist "%LIBDIR%" mkdir "%LIBDIR%"
